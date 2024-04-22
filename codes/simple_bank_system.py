168084f4ce992bb3dfa34ddba57020d41e6d0c58
alexspf <alexsandropessoa300@gmail.com>
2024-04-22 15:10:11 -0300

    bank system exercice

--- codes/simple_bank_system.py
+++ codes/simple_bank_system.py
@@ -1,65 +1,91 @@
 import os
+
+
 class UserData:
     def __init__(self, name, statement, history):
-        self.name: Any = name
-        self.statement: Any = statement
-        self.history: list = []
+        self.name = name
+        self.statement = statement
+        self.history = history
 
-user = UserData('zeca', 1000, [])  ## is a simple bank, but implemention for user is here
+
+user = UserData('zeca', 1000, [])  # is a simple bank, but implementation for user is here
+
 
 def deposit() -> None:
-    """"option to deposit"""
-    value: int = get_option(1)
+    """Option to deposit"""
+    value = get_option(1)
     user.statement += value
     user.history.append(f'{user.name} depositou {value}')
-    return
-def withdraw():
-    """"option for withdraw"""
-    while True:
-        value: int = get_option(2)
-        if value <= user.statement + 500:                ## fees :P
-            break
-        else:
-            continue
-    user.statement -= value
-    user.history.append(f'{user.name} sacou {value}')
-    return
-def statement() -> str:
-    """"option for statement"""
-    return f'o seu saldo é {user.statement}'
 
-def get_option(get) -> int:
-    """"Print options and get option for user"""
+
+def withdraw() -> None:
+    """Option for withdraw"""
+    value = get_option(2)
+    if value > user.statement + 500:  # fees :P
+        withdraw_erro()
+
+    elif value == 0:
+        operation()
+
+    else:
+        user.statement -= value
+        user.history.append(f'{user.name} sacou {value}')
+
+def withdraw_erro() -> None:
+    os.system('clear')
+    print('Saldo insuficiente para sacar esse valor, pressione um valor vazio para voltar ao menu')
+    withdraw()
+
+def statement() -> str:
+    """Option for statement"""
+    return f'O seu saldo é R${user.statement:.2f}'
+
+
+def get_option(get) -> float:
+    """Print options and get option for user"""
     try:
         match get:
-            case 0:    ## centralize string :p
-                return int(input(f'==============================================================================\nOla bien viendo {user.name} to sistema bancaro do zecatatu\n==============================================================================\nEscolha zeca\n[1]Ver saldo\n[2]depositar\n[3]sacar\n[4]Historico de operações\n[5]Sair\n==========================\n           '))
+            case 0:  # centralize string :p
+                return float(input(f'''
+==============================================================================
+Ola bien viendo {user.name} to sistema bancaro do zecatatu
+==============================================================================
+Escolha zeca
+[1]Ver saldo
+[2]Depositar
+[3]Sacar
+[4]Historico de operações
+[5]Sair
+==========================
+'''))
 
             case 1:
-                return int(input('Quanto deseja depositar?\n'))
+                return float(input('Quanto deseja depositar? R$'))
 
             case 2:
-                return int(input('Quanto deseja sacar?\n'))
+                return float(input('Quanto deseja sacar? R$'))
+
             case 3:
                 os.system('clear')
                 for x in user.history:
-                    print(f"==============================================================================\n{x}\n")
-                input(f'pressione uma tecla para sair\n')
-                return                       
-                  
+                    print(f"{x}\n")
+                input('Pressione uma tecla para sair\n')
+                return
+
+
     except Exception:
         operation()
-            
+
+
 def operation() -> None:
     while True:
         get = 0
-        option: int = get_option(get)
+        option = get_option(get)
         match option:
-
             case 1:
                 os.system('clear')
-                print (statement())
-            
+                print(statement())
+
             case 2:
                 deposit()
                 os.system('clear')
@@ -73,5 +99,7 @@ def operation() -> None:
 
             case 5:
                 break
+         
+
 
 operation()
